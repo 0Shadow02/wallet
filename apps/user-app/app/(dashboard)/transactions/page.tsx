76bc59de08a6ff98b0d8cdaa59b7cf3b07a17b26
Shadow <dark35854@gmail.com>
2024-08-11 17:45:38 +0530

    init_rm_rf

--- apps/user-app/app/(dashboard)/transactions/page.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function() {
-    return <div>
-        Transactions
-    </div>
-}
--- packages/store/src/hooks/useBalance.ts
+++
@@ -1,7 +0,0 @@
-import { useRecoilValue } from "recoil"
-import { balanceAtom } from "../atoms/balance"
-
-export const useBalance = () => {
-    const value = useRecoilValue(balanceAtom);
-    return value;
-}

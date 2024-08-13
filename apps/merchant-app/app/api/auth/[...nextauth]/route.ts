7faa652ca38d42b9667ac9fa2d37d5b0c7b30603
Shadow <dark35854@gmail.com>
2024-08-13 15:22:44 +0530

    build_workflow

--- apps/merchant-app/app/api/auth/[...nextauth]/route.ts
+++ apps/merchant-app/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,7 @@
 
-
-
-
 // import NextAuth from "next-auth"
 // import { authOptions } from "../../../../lib/auth"
+
 
 // const handler = NextAuth(authOptions)
 
--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -1,16 +1,15 @@
-// import { NextResponse } from "next/server"
-// import { PrismaClient } from "@repo/db/client";
+import { NextResponse } from "next/server"
+import prisma from "@repo/db/client"
 
-// const client = new PrismaClient();
 
-// export const GET = async () => {
-//     await client.user.create({
-//         data: {
-//             email: "asd",
-//             name: "adsads"
-//         }
-//     })
-//     return NextResponse.json({
-//         message: "hi there"
-//     })
-// }
+export const GET = async () => {
+    await prisma.user.create({
+        data: {
+            email: "asd",
+            name: "adsads"
+        }
+    })
+    return NextResponse.json({
+        message: "hi there"
+    })
+}
